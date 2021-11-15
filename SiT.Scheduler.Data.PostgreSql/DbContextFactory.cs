9c37d1f31181f6d3946ca12e08feeb6c0c39dd38
Stoil Kostov <stoil_thebest@abv.bg>
2021-11-15 12:31:20 +0200

    Setting up a Postgre database infrastructure.

---
+++ SiT.Scheduler.Data.PostgreSql/DbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiT.Scheduler.Data.PostgreSql
+{
+    class DbContextFactory
+    {
+    }
+}
