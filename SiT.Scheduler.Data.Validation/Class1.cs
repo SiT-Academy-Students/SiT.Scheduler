7bea4946a546d7d9842d7777d1f5590a2b1c8207
Miroslav Cholakov <moltenhardrock@abv.bg>
2021-11-15 17:13:37 +0200

    Asignment Attempt1
    tried to apply some changes

---
+++ SiT.Scheduler.Data.Validation/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+namespace SiT.Scheduler.Data.Validation
+{
+    public class SongValidator : AbstractValidator<Song>
+    {
+        public SongValidator()
+        {
+            this.RuleFor(x => x.Id).NotNull().WithMessage("Id cannot be empty");
+            this.RuleFor(x => x.Name).NotEmpty().WithMessage("Must Enter Song Name").Length(4, 32).WithMessage("Song name must be between 4 and 32 symbols long");
+            this.RuleFor(x => x.Author).NotEmpty().WithMessage("Must specify Song Author").Length(2, 60).WithMessage("Author's name must be between 2 and 60 symbols long");
+
+        }
+    }
+}
---
+++ SiT.Scheduler.Validation/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+namespace SiT.Scheduler.Validation
+{
+    public class Class1
+    {
+    }
+}
