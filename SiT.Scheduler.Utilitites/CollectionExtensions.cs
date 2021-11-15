04140388c654763218a4570c2aa1189f3119dcfa
Tony Troeff <tony13troev@gmail.com>
2021-11-15 21:28:06 +0200

    Fixed warnings about incorrect using directives placeent.

--- SiT.Scheduler.Utilitites/CollectionExtensions.cs
+++ SiT.Scheduler.Utilitites/CollectionExtensions.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace SiT.Scheduler.Utilitites
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public static class CollectionExtensions
     {
         public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> collection)
