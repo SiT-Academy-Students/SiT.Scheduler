47ab33768264083974e1950e133210cc15d1b27d
Radina Radoeva <radinavelizarovaradoeva@gmail.com>
2021-11-22 19:32:24 +0200

    Get and GetMany to be able to specify a projection

--- SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
+++ SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
@@ -7,11 +7,11 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IRepository<TEntity>
+    public interface IRepository<TEntity, TLayout>
         where TEntity : class
     {
-        Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
-        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
+        Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken);
+        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken);
         Task<IOperationResult> CreateAsync(TEntity entity, CancellationToken cancellationToken);
         Task<IOperationResult> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
     }
