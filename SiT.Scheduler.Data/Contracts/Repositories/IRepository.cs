23629218d646fcd4956399d2604c1ab83b7a22f3
Tony Troeff <tony13troev@gmail.com>
2021-11-22 21:23:49 +0200

    Fixed code smells

--- SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
+++ SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
@@ -1,4 +1,4 @@
-﻿namespace SiT.Scheduler.Data.Contracts.Repositories
+namespace SiT.Scheduler.Data.Contracts.Repositories
 {
     using System;
     using System.Collections.Generic;
@@ -11,8 +11,8 @@
         where TEntity : class
     {
         Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
-        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
         Task<IOperationResult<TLayout>> GetAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection, CancellationToken cancellationToken);
+        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
         Task<IOperationResult<IEnumerable<TLayout>>> GetManyAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection,CancellationToken cancellationToken);
         Task<IOperationResult> CreateAsync(TEntity entity, CancellationToken cancellationToken);
         Task<IOperationResult> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
