6198d34ba5b477a7dce3a87f919e5c07f30561e0
Radina Radoeva <radinavelizarovaradoeva@gmail.com>
2021-11-22 21:16:21 +0200

    Requested changes

--- SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
+++ SiT.Scheduler.Data/Contracts/Repositories/IRepository.cs
@@ -7,11 +7,13 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IRepository<TEntity, TLayout>
+    public interface IRepository<TEntity>
         where TEntity : class
     {
-        Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken);
-        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken);
+        Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
+        Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken);
+        Task<IOperationResult<TLayout>> GetAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection, CancellationToken cancellationToken);
+        Task<IOperationResult<IEnumerable<TLayout>>> GetManyAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection,CancellationToken cancellationToken);
         Task<IOperationResult> CreateAsync(TEntity entity, CancellationToken cancellationToken);
         Task<IOperationResult> UpdateAsync(TEntity entity, CancellationToken cancellationToken);
     }
--- SiT.Scheduler.Data/Repositories/Repository.cs
+++ SiT.Scheduler.Data/Repositories/Repository.cs
@@ -2,7 +2,8 @@
 
 namespace SiT.Scheduler.Data.Repositories
 {
-using SiT.Scheduler.Data.Contracts.Models;
+    using Microsoft.EntityFrameworkCore;
+    using SiT.Scheduler.Data.Contracts.Models;
 using SiT.Scheduler.Data.Contracts.Repositories;
 using SiT.Scheduler.Utilitites.Errors;
 using SiT.Scheduler.Utilitites.OperationResults;
@@ -13,7 +14,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-    public class Repository<TEntity, TLayout> : IRepository<TEntity, TLayout>
+    public class Repository<TEntity> : IRepository<TEntity>
        where TEntity : class
     {
         private readonly SchedulerDbContext _schedulerDbContext;
@@ -45,7 +46,7 @@ using System.Threading.Tasks;
             return operationResult;
         }
 
-        public async Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken)
+        public async Task<IOperationResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken)
         {
             var operationResult = new OperationResult<TEntity>();
             operationResult.ValidateNotNull(filter);
@@ -54,9 +55,8 @@ using System.Threading.Tasks;
 
             try
             {
-                var result = await this._schedulerDbContext.FindAsync<TEntity>(filter, cancellationToken);
-                operationResult.Data = result
-                                           .Select<TEntity, TLayout>( r => ());
+                var result = await this._schedulerDbContext.Set<TEntity>().Where(filter).FirstOrDefaultAsync(cancellationToken);
+                operationResult.Data = result;
             }
             catch (Exception e)
             {
@@ -67,7 +67,7 @@ using System.Threading.Tasks;
             return operationResult;
         }
 
-        public async Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, TLayout>> filter, CancellationToken cancellationToken)
+        public async Task<IOperationResult<IEnumerable<TEntity>>> GetManyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken)
         {
             var operationResult = new OperationResult<IEnumerable<TEntity>>();
             operationResult.ValidateNotNull(filter);
@@ -76,10 +76,8 @@ using System.Threading.Tasks;
 
             try
             {
-                var result = await this._schedulerDbContext.FindAsync<IEnumerable<TEntity>>(filter, cancellationToken);
-                operationResult.Data = result
-                                            .Select<TEntity, TLayout>( r => ())
-                                            .ToList<TEntity, TLayout>();
+                var result = await this._schedulerDbContext.Set<TEntity>().Where(filter).ToListAsync(cancellationToken);
+                operationResult.Data = result;
             }
             catch (Exception e)
             {
@@ -105,6 +103,50 @@ using System.Threading.Tasks;
                 await this._schedulerDbContext.SaveChangesAsync(cancellationToken);
             }
             catch (Exception e)
+            {
+                var error = new ErrorFromException(e);
+                operationResult.AddError(error);
+            }
+
+            return operationResult;
+        }
+
+        public async Task<IOperationResult<TLayout>> GetAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection, CancellationToken cancellationToken)
+        {
+            var operationResult = new OperationResult<TLayout>();
+            operationResult.ValidateNotNull(filter);
+            if (operationResult.IsSuccessful is false)
+                return operationResult;
+
+            try
+            {
+                var result = await this._schedulerDbContext.Set<TEntity>().Where(filter).Select(projection).FirstOrDefaultAsync(cancellationToken);
+                operationResult.Data = result;
+            }
+
+            catch (Exception e)
+            {
+                var error = new ErrorFromException(e);
+                operationResult.AddError(error);
+            }
+
+            return operationResult;
+        }
+
+        public async Task<IOperationResult<IEnumerable<TLayout>>> GetManyAsync<TLayout>(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TLayout>> projection, CancellationToken cancellationToken)
+        {
+            var operationResult = new OperationResult<IEnumerable<TLayout>>();
+            operationResult.ValidateNotNull(filter);
+            if (operationResult.IsSuccessful is false)
+                return operationResult;
+
+            try
+            {
+                var result = await this._schedulerDbContext.Set<TEntity>().Where(filter).Select(projection).ToListAsync(cancellationToken);
+                operationResult.Data = result;
+            }
+
+            catch(Exception e)
             {
                 var error = new ErrorFromException(e);
                 operationResult.AddError(error);
