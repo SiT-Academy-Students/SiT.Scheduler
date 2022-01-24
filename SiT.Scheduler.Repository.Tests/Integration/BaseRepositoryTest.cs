ae5378f26cdc69330194bfa0ecde7b9d675488a2
Tony Troeff <tony13troev@gmail.com>
2022-01-24 20:55:18 +0200

    Added tests for the repository update method.

--- SiT.Scheduler.Repository.Tests/Integration/BaseRepositoryTest.cs
+++ SiT.Scheduler.Repository.Tests/Integration/BaseRepositoryTest.cs
@@ -1,6 +1,7 @@
 namespace SiT.Scheduler.Repository.Tests.Integration;
 
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using SiT.Scheduler.Data.Contracts.Models;
@@ -8,6 +9,8 @@ using SiT.Scheduler.Data.Contracts.Repositories;
 using SiT.Scheduler.Tests;
 using SiT.Scheduler.Tests.Interface;
 using SiT.Scheduler.Utilities;
+using TryAtSoftware.Randomizer.Core;
+using TryAtSoftware.Randomizer.Core.Interfaces;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -36,6 +39,28 @@ public abstract class BaseRepositoryTest<TEntity, TDatabaseProvider> : BaseInteg
         this.Equalizer.AssertEquality(entity, getEntity.Data);
     }
 
+    [Fact]
+    public async Task UpdateShouldWorkCorrectly()
+    {
+        var originalEntity = this.PrepareEntity();
+        var createEntity = await this.Repository.CreateAsync(originalEntity, this.GetCancellationToken());
+        Assert.NotNull(createEntity);
+        Assert.True(createEntity.IsSuccessful, createEntity.ToString());
+
+        var overridenIdRandomizationRule = new RandomizationRule<TEntity, Guid>(t => t.Id, originalEntity.Id.AsConstantRandomizer());
+        var updatedEntity = this.PrepareEntity(overridenIdRandomizationRule);
+        var updateEntity = await this.Repository.UpdateAsync(updatedEntity, this.GetCancellationToken());
+        Assert.NotNull(updateEntity);
+        Assert.True(updateEntity.IsSuccessful, updateEntity.ToString());
+
+        Expression<Func<TEntity, bool>> idFilter = x => x.Id == originalEntity.Id;
+        var getEntity = await this.Repository.GetAsync(idFilter.AsEnumerable(), this.GetCancellationToken());
+        Assert.NotNull(getEntity);
+        Assert.True(getEntity.IsSuccessful, getEntity.ToString());
+
+        this.Equalizer.AssertEquality(updatedEntity, getEntity.Data);
+    }
+
     protected abstract IRepository<TEntity> Repository { get; }
-    protected abstract TEntity PrepareEntity();
+    protected abstract TEntity PrepareEntity(params IRandomizationRule<TEntity>[] overridenRules);
 }
--- SiT.Scheduler.Repository.Tests/Integration/SongRepositoryTest.cs
+++ SiT.Scheduler.Repository.Tests/Integration/SongRepositoryTest.cs
@@ -1,8 +1,12 @@
 namespace SiT.Scheduler.Repository.Tests.Integration;
+
+using System.Collections.Generic;
 using SiT.Scheduler.Data.Contracts.Repositories;
 using SiT.Scheduler.Data.Models;
 using SiT.Scheduler.Tests.Interface;
 using SiT.Scheduler.Tests.Randomization;
+using SiT.Scheduler.Utilities;
+using TryAtSoftware.Randomizer.Core.Interfaces;
 using Xunit.Abstractions;
 
 public abstract class SongRepositoryTest<TDatabaseProvider> : BaseRepositoryTest<Song, TDatabaseProvider>
@@ -15,10 +19,13 @@ public abstract class SongRepositoryTest<TDatabaseProvider> : BaseRepositoryTest
 
     protected override IRepository<Song> Repository => this.SongRepository;
 
-    protected override Song PrepareEntity()
+    protected override Song PrepareEntity(params IRandomizationRule<Song>[] overridenRules)
     {
         var instaceBuilder = new SongInstanceBuilder();
         var randomizer = new SongRandomizer(instaceBuilder);
+
+        foreach (var overridenRule in overridenRules.OrEmptyIfNull().IgnoreNullValues())
+            randomizer.OverrideRandomizationRule(overridenRule);
 
         return randomizer.PrepareRandomValue();
     }
