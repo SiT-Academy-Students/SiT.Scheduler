ea0790c756fba2929ebac30379894c7aa23c9810
Tony Troeff <tony13troev@gmail.com>
2022-02-09 20:38:24 +0200

    Working over the storage management implementaiton.

---
+++ SiT.Scheduler.StorageManagement.CloudinaryImpl/CloudinaryStorageManager.cs
@@ -0,0 +1,52 @@
+﻿namespace SiT.Scheduler.StorageManagement.CloudinaryImpl;
+
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+using SiT.Scheduler.StorageManagement.Contracts;
+using SiT.Scheduler.Utilities.Errors;
+using SiT.Scheduler.Utilities.OperationResults;
+using CloudinaryConfiguration = SiT.Scheduler.StorageManagement.CloudinaryImpl.Configuration.CloudinaryConfiguration;
+
+public class CloudinaryStorageManager : IStorageManager
+{
+    private readonly Cloudinary _cloudinary;
+    private readonly string _assetFolder;
+
+    public CloudinaryStorageManager(IOptions<CloudinaryConfiguration> cloudinaryConfiguration)
+    {
+        var config = cloudinaryConfiguration.Value ?? throw new ArgumentNullException(nameof(cloudinaryConfiguration));
+        var account = new Account(config.CloudName, config.ApiKey, config.ApiSecret);
+        this._cloudinary = new Cloudinary(account) { Api = { Secure = true } };
+        this._assetFolder = config.AssetFolder;
+    }
+
+    public async Task<IOperationResult<IFileUploadResult>> UploadFileAsync(string fileName, Stream file, CancellationToken cancellationToken)
+    {
+        var operationResult = new OperationResult<IFileUploadResult>();
+        operationResult.ValidateNotNull(fileName);
+        operationResult.ValidateNotNull(file);
+        if (!operationResult.IsSuccessful) return operationResult;
+
+        var rawUploadParams = new RawUploadParams { File = new FileDescription(fileName, file), AssetFolder = this.GetAssetFolder() };
+
+        try
+        {
+            var uploadResult = await this._cloudinary.UploadAsync(rawUploadParams, cancellationToken: cancellationToken);
+            operationResult.Data = new FileUploadResult(uploadResult.SecureUrl.AbsoluteUri);
+        }
+        catch (Exception e)
+        {
+            var error = new ErrorFromException(e);
+            operationResult.AddError(error);
+        }
+
+        return operationResult;
+    }
+
+    private string GetAssetFolder()
+    {
+        if (string.IsNullOrWhiteSpace(this._assetFolder)) return "sit/scheduler";
+        return this._assetFolder;
+    }
+}
