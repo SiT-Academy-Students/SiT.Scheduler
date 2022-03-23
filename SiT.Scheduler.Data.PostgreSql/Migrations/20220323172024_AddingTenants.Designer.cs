687cd10a4d00420d52e563a78d544c477e1b33d6
Tony Troeff <tony13troev@gmail.com>
2022-03-23 19:21:36 +0200

    Added migration for the tenants.

--- SiT.Scheduler.Data.PostgreSql/Migrations/20220323172024_AddingTenants.Designer.cs
+++ SiT.Scheduler.Data.PostgreSql/Migrations/20220323172024_AddingTenants.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SiT.Scheduler.Data.PostgreSql;
@@ -11,9 +12,10 @@ using SiT.Scheduler.Data.PostgreSql;
 namespace SiT.Scheduler.Data.PostgreSql.Migrations
 {
     [DbContext(typeof(PostgreSchedulerDbContext))]
-    partial class PostgreSchedulerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220323172024_AddingTenants")]
+    partial class AddingTenants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SiT.Scheduler.Data.PostgreSql/Migrations/20220323172024_AddingTenants.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiT.Scheduler.Data.PostgreSql.Migrations
+{
+    public partial class AddingTenants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    IsSystem = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentityTenant",
+                columns: table => new
+                {
+                    IdentitiesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TenantsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityTenant", x => new { x.IdentitiesId, x.TenantsId });
+                    table.ForeignKey(
+                        name: "FK_IdentityTenant_Identities_IdentitiesId",
+                        column: x => x.IdentitiesId,
+                        principalTable: "Identities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IdentityTenant_Tenants_TenantsId",
+                        column: x => x.TenantsId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentityTenant_TenantsId",
+                table: "IdentityTenant",
+                column: "TenantsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentityTenant");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+        }
+    }
+}
